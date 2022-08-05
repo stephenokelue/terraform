a19da5d7079a0b7207e0217dd8c3942c3ebcc868
stephenokelue <109945424+stephenokelue@users.noreply.github.com>
2022-08-05 10:06:15 +1000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
  terraform {
    backend "s3" {
-    bucket = "code-sharing-bucket"
-    key    = "avinash/s3file-3.tfstate"
-    region = "us-west-2"
+    bucket = "terraform-bucket2680"
+    key    = "state.tfstate"
+    region = "us-east-1"
     profile = "jespo"
-dynamodb_table= "firstprojecttable"
+dynamodb_table= "terraformtable"
    }
  }
 
